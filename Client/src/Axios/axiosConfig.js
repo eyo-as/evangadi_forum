bdadfc1046d76d68229e11add0a8eb6e260249eb
eyo-as <eyosiyastumiso7@gmail.com>
2024-08-21 23:12:04 +0300

    update

--- Client/src/Axios/axiosConfig.js
+++ Client/src/Axios/axiosConfig.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:7700/api",
-
+  baseURL: "http://forumbackend.deveyoas.com/api/",
   headers: {
     "Content-Type": "application/json",
   },
